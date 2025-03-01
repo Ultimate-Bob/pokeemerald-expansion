82e421b757465714076709841eb3436cf23d2b2d
Ultimate Bob <ultimate.bob808@gmail.com>
2025-03-02 03:50:12 +1100

    Add region name string table.

--- include/regions.h
+++ include/regions.h
@@ -1,8 +1,22 @@
 #ifndef GUARD_REGIONS_H
 #define GUARD_REGIONS_H
 
+#include "global.h"
 #include "constants/regions.h"
 
+extern const u8 gText_RegionKanto[];
+extern const u8 gText_RegionSevii[];
+extern const u8 gText_RegionJohto[];
+extern const u8 gText_RegionHoenn[];
+extern const u8 gText_RegionSinnoh[];
+extern const u8 gText_RegionUnova[];
+extern const u8 gText_RegionKalos[];
+extern const u8 gText_RegionAlola[];
+extern const u8 gText_RegionGalar[];
+extern const u8 gText_RegionHisui[];
+extern const u8 gText_RegionPaldea[];
+
+extern const u8 *const gRegionNames[];
 extern const u8 gMapsecToRegion[];
 
 static inline u32 GetCurrentRegion(void)
