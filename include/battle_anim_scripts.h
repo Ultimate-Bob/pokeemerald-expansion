dbe47c8480a647ca54e9a4909604cb4d61d6d985
Ultimate Bob <ultimate.bob808@gmail.com>
2025-03-03 01:00:14 +1100

    Fix from merge

--- include/battle_anim_scripts.h
+++ include/battle_anim_scripts.h
@@ -1003,6 +1003,8 @@ extern const u8 gBattleAnimGeneral_Fog[];
 extern const u8 gBattleAnimGeneral_TeraCharge[];
 extern const u8 gBattleAnimGeneral_TeraActivate[];
 extern const u8 gBattleAnimGeneral_SimpleHeal[];
+// Custom catch behaviour
+extern const u8 gBattleAnimGeneral_BallCaughtAndReturned[];
 
 // special animations
 extern const u8 gBattleAnimSpecial_LevelUp[];
