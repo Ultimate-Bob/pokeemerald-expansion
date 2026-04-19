52694d1d4fbc73857d70dfc51c44acff23914689
Ultimate Bob <ultimate.bob808@gmail.com>
2026-04-20 02:15:59 +1000

    More flare to thirst for vengeance

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -35603,19 +35603,16 @@ gBattleAnimGeneral_DynamaxGrowth:: @ PORTED FROM CFRU
 	waitforvisualfinish
 	end
 
-@ Bidoof Perfect Form
+@ feature/bidoof-perfectform
+
 gBattleAnimMove_ThirstForVengeance::
-	loadspritegfx ANIM_TAG_EXPLOSION
-	loadspritegfx ANIM_TAG_CIRCLE_OF_LIGHT
-	loadspritegfx ANIM_TAG_PINK_PETAL
-	loadspritegfx ANIM_TAG_ELECTRICITY
 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-	@ monbg ANIM_TARGET
-	createsprite gSimplePaletteBlendSpriteTemplate, ANIM_ATTACKER, 2, F_PAL_BG, 3, 0, 16, RGB_WHITE
+
+	simple_palette_blend selector=F_PAL_BG, delay=3, initial_blend_y=0, target_blend_y=16, color=RGB(29, 29, 27)
+
 	waitforvisualfinish
 	delay 10
 
-	@ createvisualtask AnimTask_SetAnimTargetToBattlerTarget, 2
 	createvisualtask AnimTask_DoomDesireLightBeam, 5
 
 	delay 9
@@ -35627,7 +35624,7 @@ gBattleAnimMove_ThirstForVengeance::
 	delay 25
 	createsprite gHorizontalLungeSpriteTemplate, ANIM_ATTACKER, 2, 4, 28
 	delay 4
-	createvisualtask AnimTask_InvertScreenColor, 2, INVERT_ALL
+	invert_screen_color scenery=INVERT_ALL
 	delay 1
 
 	playsewithpan SE_INTRO_BLAST, SOUND_PAN_ATTACKER
@@ -35693,26 +35690,86 @@ gBattleAnimMove_ThirstForVengeance::
 	delay 3
 	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
 	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 16, 16, 1, 1
+
+
+    @ a little extra lol
+	createsprite gGrowingChargeOrbSpriteTemplate, ANIM_TARGET, 2, ANIM_TARGET
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 0, 0, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 24, -24, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, -16, 16, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, -24, -12, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 16, 16, 1, 1
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 0, 0, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 24, -24, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, -16, 16, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, -24, -12, 1, 1
+	delay 3
+	playsewithpan SE_M_EXPLOSION, SOUND_PAN_TARGET
+	createsprite gExplosionSpriteTemplate, ANIM_TARGET, 3, 16, 16, 1, 1
+
+	@ splosion from light that burns the sky (original yellow color is 31,31,16)
+	monbg ANIM_TARGET
+	createvisualtask AnimTask_BlendBattleAnimPal, 10, F_PAL_TARGET, 6, 0, 16, RGB(29, 29, 27) @;Light yellow
+	call PhotonGeyserBeam
+	call LightThatBurnsTheSkyGeyser
+	call LightThatBurnsTheSkyBlast_1
+	call LightThatBurnsTheSkyGeyser
+	call LightThatBurnsTheSkyBlast_2
+	panse SE_M_SOLAR_BEAM, SOUND_PAN_ATTACKER, SOUND_PAN_TARGET, 2, 0
+	call LightThatBurnsTheSkyGeyser
+	createvisualtask AnimTask_BlendBattleAnimPal, 2, (F_PAL_BG | F_PAL_ALL_BUT_DEF), 4, 0, 16, RGB(29, 29, 27)
+	call LightThatBurnsTheSkyBlast_3
+	clearmonbg ANIM_TARGET
 	waitforvisualfinish
+
+	delay 80
+	invert_screen_color scenery=INVERT_BG
+	delay 1
+	invert_screen_color scenery=INVERT_BG
+	delay 1
+	invert_screen_color scenery=INVERT_BG
+	delay 1
+	invert_screen_color scenery=INVERT_BG
+	delay 1
+	invert_screen_color scenery=INVERT_BG
+	delay 1
+	invert_screen_color scenery=INVERT_BG
+	delay 1
 
 	playsewithpan SE_M_LEER, SOUND_PAN_TARGET
-	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0x10, 0x10
-	delay 0x2
-	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0xfff0, 0xfff0
-	createvisualtask AnimTask_InvertScreenColor, 2, INVERT_ALL
+	invert_screen_color scenery=INVERT_ALL @ reset inversion
+	invert_screen_color scenery=INVERT_BG @ must revert bg due to blend resetting it
+	invert_screen_color scenery=INVERT_ATTACKER @ must revert attacker due to blend resetting it
+	invert_screen_color scenery=INVERT_TARGET @ keep fried effect
+	createvisualtask AnimTask_BlendBattleAnimPal, 2, (F_PAL_BG | F_PAL_TARGET | F_PAL_ALL_BUT_DEF), 4, 16, 0, RGB(29, 29, 27)
 
-	delay 1
-	createsprite gSimplePaletteBlendSpriteTemplate, ANIM_ATTACKER, 2, F_PAL_BG, 3, 16, 0, RGB_WHITE
+	@ particles
+	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0x10, 0x10
+	delay 2
+	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0xfff0, 0xfff0
+
+	delay 100
 	waitforvisualfinish
-	@ clearmonbg ANIM_TARGET
 	end
 
 
 gBattleAnimMove_Compassion::
-	loadspritegfx ANIM_TAG_RAINBOW_RINGS
-	loadspritegfx ANIM_TAG_PINK_HEART
-	loadspritegfx ANIM_TAG_POISON_BUBBLE
-	loadspritegfx ANIM_TAG_SMALL_BUBBLES
 	createvisualtask AnimTask_BlendParticle, 5, ANIM_TAG_SMALL_BUBBLES, 0, 15, 15, RGB(0, 0, 255)  @Blue tear
 
 	@ ensures the target is the opposite side of this mon since this is a status move.
@@ -35755,92 +35812,4 @@ gBattleAnimMove_Compassion::
 	clearmonbg ANIM_ATTACKER
 	end
 
-@ Move_FLEUR_CANNON::
-@ 	loadspritegfx ANIM_TAG_ORBS @beam particles
-@ 	loadspritegfx ANIM_TAG_ELECTRICITY @discharge
-@ 	loadspritegfx ANIM_TAG_PINK_PETAL @pink color
-@ 	loadspritegfx ANIM_TAG_PINK_HEART @pink color
-@ 	setalpha 12, 8
-@ 	monbg ANIM_TARGET
-@ 	createvisualtask AnimTask_BlendBattleAnimPal, 0xa, F_PAL_BG, 0x1, 0x0, 0xF, 0x7440
-@ 	waitforvisualfinish
-@ 	panse SE_M_SOLAR_BEAM, SOUND_PAN_ATTACKER, SOUND_PAN_TARGET, 0x2, 0x0
-@ 	createvisualtask AnimTask_ShakeMon, 2, ANIM_ATTACKER, 0, 4, 50, 1
-@ 	createvisualtask AnimTask_FlashAnimTagWithColor, 2, ANIM_TAG_ORBS, 1, 12, RGB_RED, 16, 0, 0
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	createvisualtask AnimTask_ShakeMon2, 2, ANIM_TARGET, 4, 0, 50, 1
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	call FleurCannonBeam
-@ 	delay 0x20
-@ 	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0x10, 0x10
-@ 	delay 0x2
-@ 	createsprite gFleurCannonDischargeTemplate, ANIM_ATTACKER, 2, 0x1, 0xfff0, 0xfff0
-@ 	delay 0x5
-@ 	createvisualtask AnimTask_ShakeMon, 5, ANIM_TARGET, 0, 5, 11, 1
-@ 	waitforvisualfinish
-@ 	createvisualtask AnimTask_BlendBattleAnimPal, 0xa, F_PAL_BG, 0x1, 0x10, 0x0, 0x7440
-@ 	waitforvisualfinish
-@ 	clearmonbg ANIM_TARGET
-@ 	blendoff
-@ 	end
-@ FleurCannonBeam:
-@ 	createsprite gFleurCannonOrbTemplate, ANIM_TARGET, 2
-@ 	createsprite gFleurCannonOrbTemplate, ANIM_TARGET, 2
-@ 	delay 0x1
-@ 	createsprite gFleurCannonOrbTemplate, ANIM_TARGET, 2
-@ 	createsprite gFleurCannonOrbTemplate, ANIM_TARGET, 2
-@ 	delay 0x1
-@ 	return
-
-@ Move_CHARGE:
-@ 	loadspritegfx ANIM_TAG_ELECTRIC_ORBS
-@ 	loadspritegfx ANIM_TAG_CIRCLE_OF_LIGHT
-@ 	loadspritegfx ANIM_TAG_ELECTRICITY
-@ 	monbg ANIM_ATTACKER
-@ 	setalpha 12, 8
-@ 	createsprite gSimplePaletteBlendSpriteTemplate, ANIM_ATTACKER, 2, F_PAL_BG, 2, 0, 4, RGB_BLACK
-@ 	waitforvisualfinish
-@ 	createvisualtask AnimTask_ElectricChargingParticles, 2, ANIM_ATTACKER, 60, 2, 12
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	delay 30
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	delay 30
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	createsprite gGrowingChargeOrbSpriteTemplate, ANIM_ATTACKER, 2, 0
-@ 	delay 25
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	delay 20
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	delay 15
-@ 	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-@ 	delay 10
-@ 	delay 6
-@ 	loopsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER, 6, 5
-@ 	waitforvisualfinish
-@ 	createsprite gElectricPuffSpriteTemplate, ANIM_ATTACKER, 2, 0, 16, 16
-@ 	delay 2
-@ 	createsprite gElectricPuffSpriteTemplate, ANIM_ATTACKER, 2, 0, -16, -16
-@ 	playsewithpan SE_M_THUNDERBOLT2, SOUND_PAN_ATTACKER
-@ 	waitforvisualfinish
-@ 	createsprite gSimplePaletteBlendSpriteTemplate, ANIM_ATTACKER, 2, F_PAL_BG, 4, 4, 0, RGB_BLACK
-@ 	clearmonbg ANIM_ATTACKER
-@ 	blendoff
-@ 	end
+@ end feature/bidoof-perfect-form
